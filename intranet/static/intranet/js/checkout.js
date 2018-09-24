a8f0c687d28c7dbae34a7dfaf6f251f51457b679
Pierre ROSENZWEIG <p.rosenzweig@outlook.fr>
2018-09-24 17:38:23 +0200

    last

--- intranet/static/intranet/js/checkout.js
+++ intranet/static/intranet/js/checkout.js
@@ -1,6 +1,6 @@
 // Create a Stripe client.
 
-var stripe = Stripe('pk_test_BqdCT8ollohJHeGZY3myqxcC');
+var stripe = Stripe('pk_live_xD7Cge0fySPpjxfscQUExZsQ');
 
 // Create an instance of Elements.
 var elements = stripe.elements();
