a0c6a054c3ca55ac4a3f2c3f9ec78ea1a51446a7
Pierre ROSENZWEIG <p.rosenzweig@outlook.fr>
2018-10-09 12:29:56 +0200

    notifications bar

--- intranet/templatetags/intranet_extras.py
+++ intranet/templatetags/intranet_extras.py
@@ -1,4 +1,6 @@
 from django import template
+from datetime import datetime
+from datetime import timedelta
 
 register = template.Library()
 
@@ -17,6 +19,10 @@ def tva(value, arg):
         return None
 
 @register.filter
+def live_time(value):
+    return value + timedelta(hours=2)
+
+@register.filter
 def mois(value):
     liste_mois = ["janvier", u"février", "mars", "avril", "mai", "juin", "juillet", u"août", "septembtre", "octobre","novembre","décembre"]
     try:
@@ -31,3 +37,30 @@ def stripe(value):
         return float(value)*100
     except (ValueError, ZeroDivisionError):
         return None
+
+@register.filter(name='proper_paginate')
+def proper_paginate(paginator, current_page, neighbors=10):
+    if paginator.num_pages > 2*neighbors:
+        start_index = max(1, current_page-neighbors)
+        end_index = min(paginator.num_pages, current_page + neighbors)
+        if end_index < start_index + 2*neighbors:
+            end_index = start_index + 2*neighbors
+        elif start_index > end_index - 2*neighbors:
+            start_index = end_index - 2*neighbors
+        if start_index < 1:
+            end_index -= start_index
+            start_index = 1
+        elif end_index > paginator.num_pages:
+            start_index -= (end_index-paginator.num_pages)
+            end_index = paginator.num_pages
+        page_list = [f for f in range(start_index, end_index+1)]
+        return page_list[:(2*neighbors + 1)]
+    return paginator.page_range
+
+
+@register.simple_tag
+def url_replace(request, field, value):
+    query_string = request.GET.copy()
+    query_string[field] = value
+
+    return query_string.urlencode()
